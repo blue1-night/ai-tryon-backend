be9a9825824e98e9efdbf5a55a9935f4714cf822
blue1-night <bluenightblue12@example.com>
2026-05-20 22:16:19 +0000

    fix production VPS deployment

--- config.py
+++ config.py
@@ -4,8 +4,16 @@ import os
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 class Config:
-    SECRET_KEY = os.environ.get("SECRET_KEY", "dev-secret-key")
-    JWT_SECRET_KEY = os.environ.get("JWT_SECRET_KEY", "jwt-secret-key")
+    SECRET_KEY = os.environ.get(
+        "SECRET_KEY",
+        "ai_tryon_super_secure_secret_key_2026_very_long"
+    )
+
+    JWT_SECRET_KEY = os.environ.get(
+        "JWT_SECRET_KEY",
+        "ai_tryon_jwt_super_secure_secret_key_2026_very_long"
+    )
+
     JWT_ACCESS_TOKEN_EXPIRES = timedelta(hours=8)
 
     SQLALCHEMY_DATABASE_URI = f"sqlite:///{os.path.join(BASE_DIR,'data.sqlite')}"
@@ -14,11 +22,14 @@ class Config:
     UPLOAD_FOLDER = os.path.join(BASE_DIR, "uploads")
     OUTPUT_FOLDER = os.path.join(BASE_DIR, "outputs")
 
-    STATIC_CLOTHING = os.path.join(BASE_DIR, "static", "clothing")
+    STATIC_CLOTHING = os.environ.get(
+        "STATIC_CLOTHING",
+        "/var/www/ai-tryon/static/clothing"
+    )
 
     BASE_URL = os.environ.get(
         "BASE_URL",
         "http://103.55.38.102"
     )
 
-    MAX_CONTENT_LENGTH = 20 * 1024 * 1024
+    MAX_CONTENT_LENGTH = 50 * 1024 * 1024
